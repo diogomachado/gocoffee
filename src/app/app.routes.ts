4558fa0908b71ccb6cb97d6fc15da9067ca9e995
Diogo Machado <diogo@diogomachado.com>
2024-08-18 17:59:38 -0300

    feat: lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'como-funciona', component: AboutComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'como-funciona',
+    loadComponent() {
+      return import('./pages/about/about.component').then(
+        (m) => m.AboutComponent
+      );
+    },
+  },
+  {
+    path: 'login',
+    loadComponent() {
+      return import('./pages/login/login.component').then(
+        (m) => m.LoginComponent
+      );
+    },
+  },
 ];
