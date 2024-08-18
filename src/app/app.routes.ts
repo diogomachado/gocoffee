a605f5c8d87fcae2eb21f371cad7af748db8d418
Diogo Machado <diogo@diogomachado.com>
2024-08-18 17:43:21 -0300

    feat: primeiras páginas e rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'como-funciona', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+];
